0aa0873f699ef7c127f6bc5f8e4f57ac6224df0e
Andrew Vota <andrew@vota.cc>
2024-02-07 06:23:14 -0500

    Disable mouse and arrow keys

---
+++ lua/keymaps.lua
@@ -0,0 +1,19 @@
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Unset arrow keys
+vim.cmd [[
+    noremap <Left> <Nop>
+    noremap <Right> <Nop>
+    noremap <Up> <Nop>
+    noremap <Down> <Nop>
+
+
+    inoremap <Left> <Nop>
+    inoremap <Right> <Nop>
+    inoremap <Up> <Nop>
+    inoremap <Down> <Nop>
+]]
+
+-- Disable mouse
+vim.cmd [[ set mouse= ]]
