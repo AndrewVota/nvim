00c06c3f22cf6f70f940e111ed7bc50a90ff5932
Andrew Vota <andrew@vota.cc>
2024-02-19 13:08:54 -0500

    Everforest config fix

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -2,7 +2,7 @@ return {
     "neanias/everforest-nvim",
     version = false,
     lazy = false,
-    priorty = 1000,
+    priority = 1000,
     config = function()
         vim.cmd.colorscheme("everforest")
     end
