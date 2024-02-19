883bb44e2b6e5284389d94c48b8d5126d12c6624
Andrew Vota <andrew@vota.cc>
2024-02-19 09:47:34 -0500

    Disable the arrow key unset

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,18 +2,18 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Unset arrow keys
-vim.cmd([[
-    noremap <Left> <Nop>
-    noremap <Right> <Nop>
-    noremap <Up> <Nop>
-    noremap <Down> <Nop>
+-- vim.cmd([[
+--  noremap <Left> <Nop>
+--  noremap <Right> <Nop>
+--  noremap <Up> <Nop>
+--  noremap <Down> <Nop>
 
 
-    inoremap <Left> <Nop>
-    inoremap <Right> <Nop>
-    inoremap <Up> <Nop>
-    inoremap <Down> <Nop>
-]])
+--  inoremap <Left> <Nop>
+--  inoremap <Right> <Nop>
+--  inoremap <Up> <Nop>
+--    inoremap <Down> <Nop>
+-- ]])
 
 -- Disable mouse
-vim.cmd([[ set mouse= ]])
+-- vim.cmd([[ set mouse= ]])
