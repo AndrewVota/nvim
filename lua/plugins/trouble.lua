95bc0e89cc196fb1d8fb8f14b4eb6daa8786f2c1
Andrew Vota <andrew@vota.cc>
2024-02-20 16:45:23 -0500

    Map `:Trouble` to `<leader>ow`

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,4 +2,7 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
+    keys = {
+        { "<leader>ow", "<cmd>Trouble<cr>", desc = "Trouble" },
+    }
 }
