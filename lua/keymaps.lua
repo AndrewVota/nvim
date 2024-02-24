1d94f36b4826f3c3228ac830aa27fb4d3fa4c8c8
Andrew Vota <andrew@vota.cc>
2024-02-24 14:01:07 -0500

    New `vsplit` and navigation keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,15 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Remap jk to <ESC> to exit insert mode
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true})
 
+-- Use ctrl-v to make a new vsplit
+vim.api.nvim_set_keymap('n', '<C-v>', ':vsplit<CR>', {noremap = true, silent = true})
+
+-- Use ctrl-[hjkl] to select the active split in Neovim using Lua
+vim.api.nvim_set_keymap('n', '<c-k>', '<cmd>wincmd k<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<c-j>', '<cmd>wincmd j<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<c-h>', '<cmd>wincmd h<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<c-l>', '<cmd>wincmd l<CR>', {silent = true, noremap = true})
+
 -- Unset arrow keys
 -- vim.cmd([[
 --  noremap <Left> <Nop>
