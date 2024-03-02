56119334ec95d653396689d21b57f208e5e7dc18
Andrew Vota <andrew@vota.cc>
2024-03-02 14:19:08 -0500

    Enable `catppuccin.nvim` with transparent background color

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,14 @@
 return {
 	"catppuccin/nvim",
-    enabled = false,
+    enabled = true,
 	lazy = false,
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+        local catppuccin = require("catppuccin")
+        catppuccin.setup({
+            transparent_background = true,
+        })
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
