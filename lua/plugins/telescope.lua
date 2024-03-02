9eac1bb296be9b683dac91a498f228ad3bf61d46
Andrew Vota <andrew@vota.cc>
2024-03-02 10:23:13 -0500

    Add `telescope-file-browser` keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,5 +25,17 @@ return {
 	{
 		"nvim-telescope/telescope-file-browser.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["file_browser"] = {
+						require("telescope.themes").get_dropdown({}),
+                        hijack_netrw = true,
+					},
+				},
+			})
+			require("telescope").load_extension("file_browser")
+            vim.keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<CR>", { noremap = true })
+		end,
 	},
 }
