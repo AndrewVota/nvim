d35f4790f113477d642957a735e090210e1cc821
Andrew Vota <andrew@vota.cc>
2024-03-03 09:34:11 -0500

    feat: Options, Keymaps, and Lazy package manager

---
+++ lua/keymaps.lua
@@ -0,0 +1,22 @@
+-- Clear search highlight with <Esc> in normal mode
+vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<LEADER>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<LEADER>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Center screen while cycling
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Remap jk / kj to <ESC> to exit insert mode
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", {noremap = true})
+vim.api.nvim_set_keymap("i", "kj", "<ESC>", {noremap = true})
+
+-- Navigate splits with <CTRL> hjkl
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
