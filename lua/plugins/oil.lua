ffb807df5ebf697214fe339aae637bd8459b66a3
Andrew Vota <andrew@vota.cc>
2024-03-07 07:49:16 -0500

    feat: Show hidden files in `oil.nvim`
    chore: `:LazyUpdate`

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,5 +2,12 @@ return {
 	"stevearc/oil.nvim",
 	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
+	end,
 	vim.keymap.set("n", "<C-o>", "<CMD>Oil<CR>", { desc = "Trigger Oil file explorer buffer" }),
 }
